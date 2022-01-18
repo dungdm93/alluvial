8769805333a6f785d334627e076a420b337d23e8
Đặng Minh Dũng <dungdm93@live.com>
2022-01-18 14:47:13 +0700

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,38 @@
 plugins {
+    application
     java
+    kotlin("jvm") version "1.6.10"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "dev.alluvial"
 version = "0.1-SNAPSHOT"
 
+java.toolchain {
+    languageVersion.set(JavaLanguageVersion.of(11))
+}
+
 repositories {
     mavenCentral()
 }
 
+val slf4jVersion = "1.7.36"
+val logbackVersion = "1.2.10"
+
+val junitVersion = "5.8.2"
+val striktVersion = "0.33.0"
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0")
+    api("org.slf4j:slf4j-api:$slf4jVersion")
+    runtimeOnly("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Test Frameworks
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation("org.junit.vintage:junit-vintage-engine:$junitVersion")
+    testImplementation("io.strikt:strikt-jvm:$striktVersion")
 }
 
 tasks.getByName<Test>("test") {
