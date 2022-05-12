d4dfc9082e1ac8a632a0d2da7a4b08c384218273
Thai Quang Tien <tien.tq@teko.vn>
2022-05-12 10:37:37 +0700

    [DI-1066] Configure partition name

--- src/main/java/org/apache/iceberg/PartitionSpecs.java
+++ src/main/java/org/apache/iceberg/PartitionSpecs.java
@@ -3,14 +3,22 @@ package org.apache.iceberg;
 import org.apache.iceberg.PartitionSpec.Builder;
 import org.apache.iceberg.types.Types;
 
+import java.util.Locale;
+
 public class PartitionSpecs {
-    public static void add(Builder builder, Types.NestedField field, String transform) {
-        String targetName;
-        if (transform.contains("[")) {
-            targetName = field.name() + "_" + transform.substring(0, transform.indexOf('['));
-        } else {
-            targetName = field.name() + "_" + transform;
+    public static void add(Builder builder, Types.NestedField field, String transform, String name) {
+        String targetName = name;
+        if (name == null || name.isEmpty()) {
+            if (transform.contains("[")) {
+                targetName = field.name() + "_" + transform.substring(0, transform.indexOf('['));
+            } else {
+                targetName = field.name() + "_" + transform;
+            }
         }
-        builder.add(field.fieldId(), targetName, transform);
+        builder.add(field.fieldId(), targetName.toLowerCase(), transform);
+    }
+
+    public static void add(Builder builder, Types.NestedField field, String transform) {
+        add(builder, field, transform, null);
     }
 }
