a11b672b2b283803dd8d28922a61bda34f6e2dc5
Thai Quang Tien <tien.tq@teko.vn>
2022-06-23 15:47:43 +0700

    [DI-1228] Preload logical type providers to avoid race condition

--- src/main/kotlin/dev/alluvial/sink/iceberg/type/logical/extension.kt
+++ src/main/kotlin/dev/alluvial/sink/iceberg/type/logical/extension.kt
@@ -3,11 +3,15 @@ package dev.alluvial.sink.iceberg.type.logical
 import dev.alluvial.sink.iceberg.type.KafkaSchema
 import java.util.ServiceLoader
 
-private var logicalTypeProviderLoader = ServiceLoader.load(LogicalTypeProvider::class.java)
+/**
+ * Since ServiceLoader is not thread-safe, preload all providers to avoid error
+ * @see java.util.ServiceLoader
+ */
+private val logicalTypeProviders = ServiceLoader.load(LogicalTypeProvider::class.java).toList()
 
 fun KafkaSchema.logicalTypeConverter(): LogicalTypeConverter<*, *>? {
     val logicalName = this.name() ?: return null
-    logicalTypeProviderLoader.forEach { provider ->
+    logicalTypeProviders.forEach { provider ->
         val converter = provider.getConverter(logicalName)
         if (converter != null) return converter
     }
