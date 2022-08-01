3f3188c884e87b7aa99aa802b9e434ef5b27c0f6
Thai Quang Tien <tien.tq@teko.vn>
2022-08-01 13:56:33 +0700

    Set snapshot extra metadata

--- src/main/kotlin/dev/alluvial/sink/iceberg/helpers.kt
+++ src/main/kotlin/dev/alluvial/sink/iceberg/helpers.kt
@@ -3,11 +3,13 @@ package dev.alluvial.sink.iceberg
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.json.JsonMapper
 import dev.alluvial.sink.iceberg.type.IcebergTable
+import org.apache.iceberg.SnapshotSummary
 
 internal val mapper = JsonMapper()
 internal val offsetsTypeRef = object : TypeReference<Map<Int, Long>>() {}
-internal const val ALLUVIAL_POSITION_PROP = "alluvial.position"
-internal const val ALLUVIAL_LAST_RECORD_TIMESTAMP_PROP = "alluvial.last-record.timestamp"
+internal const val SUMMARY_PREFIX = SnapshotSummary.EXTRA_METADATA_PREFIX
+internal const val ALLUVIAL_POSITION_PROP = "${SUMMARY_PREFIX}alluvial.position"
+internal const val ALLUVIAL_LAST_RECORD_TIMESTAMP_PROP = "${SUMMARY_PREFIX}alluvial.last-record.timestamp"
 
 fun IcebergTable.committedOffsets(): Map<Int, Long> {
     val serialized = this.currentSnapshot()
