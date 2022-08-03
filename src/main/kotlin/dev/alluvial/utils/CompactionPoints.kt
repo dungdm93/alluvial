89233ede3bc002f081f0dfd8907d1872e362c047
Đặng Minh Dũng <dungdm93@live.com>
2022-08-03 11:02:47 +0700

    add `CompactionPoints`
    CompactionPoints are instants that start evaluate to be compact for each profile

---
+++ src/main/kotlin/dev/alluvial/utils/CompactionPoints.kt
@@ -0,0 +1,66 @@
+package dev.alluvial.utils
+
+import dev.alluvial.runtime.CompactionRules
+import org.apache.iceberg.Snapshot
+import org.apache.iceberg.originalTimestampMillis
+import java.time.Instant
+import java.time.LocalTime
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
+class CompactionPoints(
+    val tz: ZoneOffset,
+    val hourCompactionPoint: Instant,
+    val dayCompactionPoint: Instant,
+    val monthCompactionPoint: Instant,
+    val yearCompactionPoint: Instant,
+) {
+    companion object {
+        private val RAW_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+        private val HOUR_COMPACT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH")
+        private val DAY_COMPACT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        private val MONTH_COMPACT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM")
+        private val YEAR_COMPACT_FORMATTER = DateTimeFormatter.ofPattern("yyyy")
+
+        fun from(now: ZonedDateTime, rules: CompactionRules): CompactionPoints {
+            val midnight = now.with(LocalTime.MIDNIGHT)
+
+            val hourCompactionPoint = now.minusHours(rules.retainRaw)
+                .truncatedTo(ChronoUnit.HOURS)
+                .toInstant()
+            val dayCompactionPoint = now.minusHours(rules.retainHourCompact)
+                .with(LocalTime.MIDNIGHT)
+                .toInstant()
+
+            val monthCompactionPoint = midnight.minusDays(rules.retainDayCompact)
+                .withDayOfMonth(1)
+                .toInstant()
+            val yearCompactionPoint = midnight.minusMonths(rules.retainMonthCompact)
+                .withDayOfYear(1)
+                .toInstant()
+
+            return CompactionPoints(
+                rules.tz,
+                hourCompactionPoint,
+                dayCompactionPoint,
+                monthCompactionPoint,
+                yearCompactionPoint,
+            )
+        }
+    }
+
+    fun keyOf(snapshot: Snapshot): String {
+        val ts = Instant.ofEpochMilli(snapshot.originalTimestampMillis())
+        val zdt = ZonedDateTimes.ofEpochTime(snapshot.originalTimestampMillis(), TimePrecision.MILLIS, tz)
+
+        return when {
+            hourCompactionPoint <= ts -> RAW_FORMATTER.format(zdt)
+            dayCompactionPoint <= ts -> HOUR_COMPACT_FORMATTER.format(zdt)
+            monthCompactionPoint <= ts -> DAY_COMPACT_FORMATTER.format(zdt)
+            yearCompactionPoint <= ts -> MONTH_COMPACT_FORMATTER.format(zdt)
+            else -> YEAR_COMPACT_FORMATTER.format(zdt)
+        }
+    }
+}
---
+++ src/test/kotlin/dev/alluvial/utils/TestCompactionPoints.kt
@@ -0,0 +1,102 @@
+package dev.alluvial.utils
+
+import dev.alluvial.runtime.CompactionRules
+import org.apache.iceberg.Snapshot
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import java.time.ZonedDateTime
+
+internal class TestCompactionPoints {
+    @Test
+    fun defaultCompactionRules() {
+        val rules = CompactionRules()
+        val now = ZonedDateTime.parse("2022-07-15T12:14:16.432Z")
+        val points = CompactionPoints.from(now, rules)
+
+        Assert.assertEquals(
+            ZonedDateTime.parse("2022-07-15T09:00:00Z").toInstant(),
+            points.hourCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2022-07-14T00:00:00Z").toInstant(),
+            points.dayCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2022-06-01T00:00:00Z").toInstant(),
+            points.monthCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2021-01-01T00:00:00Z").toInstant(),
+            points.yearCompactionPoint
+        )
+    }
+
+    @Test
+    fun compactionPointsInSameTimeUnit() {
+        val rules = CompactionRules(
+            retainRaw = 3,
+            retainHourCompact = 12,
+            retainDayCompact = 15,
+            retainMonthCompact = 6
+        )
+        val now = ZonedDateTime.parse("2021-12-25T19:20:45.078Z")
+        val points = CompactionPoints.from(now, rules)
+        Assert.assertEquals(
+            ZonedDateTime.parse("2021-12-25T16:00:00Z").toInstant(),
+            points.hourCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2021-12-25T00:00:00Z").toInstant(),
+            points.dayCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2021-12-01T00:00:00Z").toInstant(),
+            points.monthCompactionPoint
+        )
+        Assert.assertEquals(
+            ZonedDateTime.parse("2021-01-01T00:00:00Z").toInstant(),
+            points.yearCompactionPoint
+        )
+    }
+
+    @Test
+    fun getKeyOfSnapshot() {
+        val rules = CompactionRules()
+        val now = ZonedDateTime.parse("2022-07-15T12:14:16.432Z")
+        val points = CompactionPoints.from(now, rules)
+
+        // Raw
+        testKeyFor(points, "2022-07-15T10:00:00Z", "2022-07-15T10:00:00")
+        testKeyFor(points, "2022-07-15T09:00:00Z", "2022-07-15T09:00:00")
+
+        // Hour
+        testKeyFor(points, "2022-07-15T08:59:59Z", "2022-07-15T08")
+        testKeyFor(points, "2022-07-14T00:00:00Z", "2022-07-14T00")
+
+        // Day
+        testKeyFor(points, "2022-07-13T23:59:59Z", "2022-07-13")
+        testKeyFor(points, "2022-06-01T00:00:00Z", "2022-06-01")
+
+        // Month
+        testKeyFor(points, "2022-05-31T23:59:59Z", "2022-05")
+        testKeyFor(points, "2021-01-01T00:00:00Z", "2021-01")
+
+        // Year
+        testKeyFor(points, "2020-12-31T23:59:59Z", "2020")
+        testKeyFor(points, "2020-05-06T07:08:09Z", "2020")
+    }
+
+    private fun testKeyFor(points: CompactionPoints, input: String, key: String) {
+        val ts = ZonedDateTime.parse(input).toInstant().toEpochMilli()
+        val snapshot = snapshotOfTimestamp(ts)
+        Assert.assertEquals(key, points.keyOf(snapshot))
+    }
+
+    private fun snapshotOfTimestamp(timestampMillis: Long): Snapshot {
+        return mock {
+            on { timestampMillis() } doReturn timestampMillis
+        }
+    }
+}
