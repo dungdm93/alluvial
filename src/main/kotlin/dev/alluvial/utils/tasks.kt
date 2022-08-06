52a19b475676c31e77b0b20865a76370a1740efd
Đặng Minh Dũng <dungdm93@live.com>
2022-08-06 17:18:02 +0700

    add `LanePoolRunner`
    A wrapper of [ExecutorService] that all tasks in the same group must be run in order - it's similar to the swim lane.

---
+++ src/main/kotlin/dev/alluvial/utils/tasks.kt
@@ -0,0 +1,47 @@
+package dev.alluvial.utils
+
+fun interface Task<I, O> {
+    fun run(input: I): O
+}
+
+interface Callback<I, O> {
+    fun onSuccess(input: I, result: O)
+    fun onFailure(input: I, throwable: Throwable)
+}
+
+class CallbackTask<I, O>(
+    private val task: Task<I, O>
+) : Task<I, O> {
+    private val callbacks = mutableListOf<Callback<I, O>>()
+
+    fun addCallback(callback: Callback<I, O>) {
+        callbacks.add(callback)
+    }
+
+    override fun run(input: I): O {
+        try {
+            val output = task.run(input)
+            onSuccess(input, output)
+            return output
+        } catch (throwable: Throwable) {
+            onFailure(input, throwable)
+            throw throwable
+        }
+    }
+
+    private fun onSuccess(input: I, output: O) {
+        for (callback in callbacks) {
+            callback.onSuccess(input, output)
+        }
+    }
+
+    private fun onFailure(input: I, throwable: Throwable) {
+        for (callback in callbacks) {
+            try {
+                callback.onFailure(input, throwable)
+            } catch (t: Throwable) {
+                throwable.addSuppressed(t)
+            }
+        }
+    }
+}
